5e47132f70d3893826677b624b0dd7c6793ac72c
Declan Stacy <88392873+DeclanStacy@users.noreply.github.com>
2022-03-26 15:19:55 -0400

    Add files via upload
    Fix syntax

--- CoupDeck.py
+++ CoupDeck.py
@@ -1,17 +1,17 @@
 import random
 
 class CoupDeck:
-    def __init__():
+    def __init__(self):
         self.deck = [0,1,2,3,4]*3
         deck.shuffle()
 
-    def shuffle():
+    def shuffle(self):
         self.deck.shuffle()
 
-    def draw():
+    def draw(self):
         return deck.pop()
 
-    def add(card1, card2 = -1):
+    def add(self, card1, card2 = -1):
         self.deck.append(card1)
         if card2 != -1:
             self.deck.append(card2)
