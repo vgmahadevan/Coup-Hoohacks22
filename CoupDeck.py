ba25bed51f2922fbb58cf3fac03949827f5ee6d2
Vagul Mahadevan <vagulgm@gmail.com>
2022-03-26 16:33:12 -0400

    Fixed missing "self" keywords

--- CoupDeck.py
+++ CoupDeck.py
@@ -3,13 +3,13 @@ import random
 class CoupDeck:
     def __init__(self):
         self.deck = [0,1,2,3,4]*3
-        deck.shuffle()
+        self.deck.shuffle()
 
     def shuffle(self):
         self.deck.shuffle()
 
     def draw(self):
-        return deck.pop()
+        return self.deck.pop()
 
     def add(self, card1, card2 = -1):
         self.deck.append(card1)
