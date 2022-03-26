9b442be35a487084fc698c70c9fffef55c16a690
Declan Stacy <88392873+DeclanStacy@users.noreply.github.com>
2022-03-26 15:02:50 -0400

    Add files via upload
    Deck class. Don't call shuffle method

---
+++ CoupDeck.py
@@ -0,0 +1,18 @@
+import random
+
+class CoupDeck:
+    def __init__():
+        self.deck = [0,1,2,3,4]*3
+        deck.shuffle()
+
+    def shuffle():
+        self.deck.shuffle()
+
+    def draw():
+        return deck.pop()
+
+    def add(card1, card2 = -1):
+        self.deck.append(card1)
+        if card2 != -1:
+            self.deck.append(card2)
+        self.deck.shuffle()
